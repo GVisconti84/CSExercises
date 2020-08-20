27b34dab9dae5a1771a1dced0bf4494e87482535
JohnBala <kilbanine@gmail.com>
2020-08-20 20:58:44 +0200

    new excercises

---
+++ CSExercises/Exercise_4_10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSExercises
+{
+    public class GradeBook
+    {
+        public string CourseName { get; set; }
+        public string TeacherName { get;  set; }
+
+        public GradeBook(string courseName, string teacherName)
+        {
+            CourseName = courseName;
+            TeacherName = teacherName;
+        }
+
+        
+
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the grade book for: {0}", CourseName);
+            Console.WriteLine("The course is presented by: {0}", TeacherName);
+        }
+    }
+}
---
+++ CSExercises/Exercise_4_11.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSExercises
+{
+    public class Account
+    {
+        private decimal balance;
+        
+
+        public Account( decimal initialBalance )
+        {
+            balance = initialBalance;
+        }
+
+        public void Credit( decimal amount )
+        {
+            balance = balance + amount;
+        }
+
+        public void Debit (decimal amount)
+        {
+            if (amount <= balance)
+                balance = balance - amount;
+            else Console.WriteLine("Debit amount exceeded account balance.");
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                if (value >= 0)
+                    balance = value;
+            }
+        }
+
+      
+    }
+}
