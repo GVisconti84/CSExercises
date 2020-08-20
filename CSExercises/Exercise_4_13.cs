fad1d0db09722eb15a777a0655968611a462829c
JohnBala <kilbanine@gmail.com>
2020-08-20 22:37:33 +0200

    Added exercise 4_13

---
+++ CSExercises/Exercise_4_13.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSExercises
+{
+    public class Employee
+    {
+        private decimal salary;
+        private decimal yearlySalary;
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        
+        public decimal mSalary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (value >= 0)
+                    mSalary = value;
+            }
+        }
+
+
+        public Employee(string firstName, string lastName, decimal mSalary)
+        {
+            
+            firstName = firstName;
+            lastName = lastName;
+            salary = mSalary;
+        }
+
+        public decimal YearlySalary(decimal salary)
+        {
+            yearlySalary = salary * 12;
+            return yearlySalary;
+        }
+    }
+}
