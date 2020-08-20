b512c1edace8b70f463937e6cec6a842a020bf36
JohnBala <kilbanine@gmail.com>
2020-08-20 22:37:55 +0200

    Cleaned the main

--- CSExercises/Program.cs
+++ CSExercises/Program.cs
@@ -9,57 +9,15 @@ namespace CSExercises
             //BMICalculator.LocalMain(args);
             //Chapeter3ex.LocalMain(args);
 
-            /* Excercise 4_10
-            GradeBook gradeBook = new GradeBook("Corso di c# fatto serio", "Gabriele Visconti");
-            gradeBook.DisplayMessage();
-            */
 
 
-            /*Excercise 4_11
-            Account account1 = new Account(50.00M);
-            Account account2 = new Account(-7.53M);
-
-            Console.WriteLine("account1 balance: {0:C}", account1.Balance);
-            Console.WriteLine("account2 balance: {0:C}", account2.Balance);
-
-            Console.WriteLine("Insert deposit amount for account1: ");
-            decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("adding {0:C} to account1 balance", depositAmount);
-            account1.Credit(depositAmount);
-
-            Console.WriteLine("account1 balance: {0:C}", account1.Balance);
-            Console.WriteLine("account2 balance: {0:C}", account2.Balance);
-
-
-            Console.WriteLine("Insert deposit amount for account2: ");
-            depositAmount = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("adding {0:C} to account2 balance", depositAmount);
-            account2.Credit(depositAmount);
-
-            Console.WriteLine("account1 balance: {0:C}", account1.Balance);
-            Console.WriteLine("account2 balance: {0:C}", account2.Balance);
-
-            Console.WriteLine("Insert withdraw amount for account1: ");
-            decimal withdrawAmount = Convert.ToDecimal(Console.ReadLine());
-            account1.Debit(withdrawAmount);
-
-            Console.WriteLine("account1 balance: {0:C}", account1.Balance);
-            Console.WriteLine("account2 balance: {0:C}", account2.Balance);
-
-            */
-
-
-            /*4_12*/
-
-            Invoice fattura = new Invoice("43", "Pen Drive", 5, 15.31M);
-
-            Console.WriteLine("Invoice Number: {0}", fattura.Number);
-            Console.WriteLine("Invoice Description: {0}", fattura.Description);
-            Console.WriteLine("Quantity: {0}", fattura.Quantity);
-            Console.WriteLine("Price Per Item: {0:C}\n", fattura.PricePerItem);
-            Console.WriteLine("Total Invoice: {0:C}", fattura.GetInvoiceAmount(fattura.Quantity, fattura.PricePerItem));
+            //GradeBookTest.LocalMain(args);
+            //AccountTest.LocalMain(args);
+            //InvoiceTest.LocalMain(args);
+            //EmployeeTest.LocalMain(args);
            
 
+            
         }
     }
 }
