43cf33d245b79fa1d721360ca06f6e09a90b5884
JohnBala <kilbanine@gmail.com>
2020-08-21 11:23:50 +0200

    Exercise 4_14 and 4_14_Test

---
+++ CSExercises/Exercise_4_14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSExercises
+{
+    class Date
+    {
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public int Year { get; set; }
+
+        public Date (int day, int month, int year)
+        {
+            Month = month;
+            Day = day;
+            Year = year;
+        }
+
+        public void DisplayDate ()
+        {
+             Console.WriteLine("{0} / {1} / {2} ", Convert.ToString(Day), Convert.ToString(Month), Convert.ToString(Year));
+        }
+    }
+}
---
+++ CSExercises/Exercise_4_14_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSExercises
+{
+     class Date_Test
+    {
+        public static void LocalMain(string[] args)
+        {
+            Date date = new Date(12, 9, 1997);
+
+            date.DisplayDate();
+        }
+    }
+}
